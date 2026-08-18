14359cce9dbafddbb7386b20c8d466456b440286
grepfox <grepfox@tutamail.com>
2026-08-18 16:24:24 +0000

    LineageParts: DynamicIsland: Add Live Sports toggle and Live Sports launcher
    Change-Id: I793e04e4d1ae1bc05a4c4859ca2cd8e9c6c246f6

--- src/org/lineageos/lineageparts/statusbar/DynamicIslandSettings.java
+++ src/org/lineageos/lineageparts/statusbar/DynamicIslandSettings.java
@@ -11,10 +11,29 @@ import org.lineageos.lineageparts.SettingsPreferenceFragment;
 
 public class DynamicIslandSettings extends SettingsPreferenceFragment {
 
+    private static final String KEY_PINNED_SPORTS = "status_bar_dynamic_island_pinned_sports";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.dynamic_island_settings);
         getActivity().setTitle(R.string.status_bar_dynamic_island_title);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(androidx.preference.Preference preference) {
+        if (KEY_PINNED_SPORTS.equals(preference.getKey())) {
+            android.content.Intent intent = new android.content.Intent();
+            intent.setComponent(new android.content.ComponentName(
+                    "org.lineageos.sportsfetcher", "org.lineageos.sportsfetcher.MainActivity"));
+            intent.addFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK);
+            try {
+                startActivity(intent);
+                return true;
+            } catch (android.content.ActivityNotFoundException e) {
+                // Fallback to default
+            }
+        }
+        return super.onPreferenceTreeClick(preference);
     }
 }
